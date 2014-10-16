ed6c09851f2450572be8146b5acf29e4991e5320
Uladzimir_Harabtsou@epam.com <Uladzimir_Harabtsou@epam.com>
2014-10-16 09:42:07 +0300

    fix

---
+++ Jodata/JiraEntities/Issue.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Services.Common;
+using Newtonsoft.Json;
+
+namespace Jodata.JiraEntities
+{
+  [DataServiceKey("Id")]
+  public class Issue : BaseEntity
+  {
+    [JsonProperty("expand")]
+    public string Expand { get; set; }
+
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("key")]
+    public string Key { get; set; }
+
+    [JsonProperty("fields")]
+    public Fields Fields { get; set; }
+  }
+}
